8e3c3195771f5beb7c99787f121ada1e2669187d
stevenszhou20 <33017597+stevenszhou20@users.noreply.github.com>
2017-10-27 08:28:26 +0800

    Create Room.cpp

---
+++ Room.cpp
@@ -0,0 +1,17 @@
+/*
+ * Room.cpp
+ *
+ *  Created on: 2017年10月25日
+ *      Author: 18962
+ */
+
+#include "Room.h"
+
+Room::Room(int n,int d,int p,int s) :number(n),dank(d),price(p),state(s){
+	// TODO Auto-generated constructor stub
+
+}
+
+Room::~Room() {
+	// TODO Auto-generated destructor stub
+}
